8ed47998bf4614ff169a815947a1abd17f882175
furkanagess <caglarrfurkann@gmail.com>
2023-07-03 23:19:41 +0300

    signupPage widgets extracted

--- lib/feature/view/auth/signup_view.dart
+++ lib/feature/view/auth/signup_view.dart
@@ -1,4 +1,5 @@
 import 'package:car_store_app/product/constants/app_colors.dart';
+import 'package:car_store_app/product/constants/app_strings.dart';
 import 'package:car_store_app/product/extension/context_extension.dart';
 import 'package:car_store_app/product/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
@@ -14,151 +15,197 @@ class SignupView extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: context.paddingNormal,
-          child: Column(
+          child: const Column(
             children: [
               Expanded(
                 flex: 2,
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: AppColors.orange,
-                      radius: 45,
-                      child: Text(
-                        "C",
-                        style: context.textTheme.headlineLarge?.copyWith(
-                          color: AppColors.background,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: context.dynamicHeight(0.05),
-                    ),
-                    Text(
-                      "Sign Up",
-                      style: context.textTheme.headlineSmall?.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "Find your dream car!",
-                      style: context.textTheme.bodyLarge,
-                    ),
-                  ],
-                ),
+                child: HeaderLogoText(),
               ),
               Expanded(
                 flex: 3,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    CustomTextField(hintText: "Full name", icon: Icons.person_outline),
-                    CustomTextField(hintText: "Email address", icon: Icons.mail_outline),
-                    CustomTextField(hintText: "Phone umber", icon: Icons.phone_outlined),
-                    CustomTextField(hintText: "Password", icon: Icons.lock_outline),
-                  ],
-                ),
+                child: TextfieldArea(),
               ),
               Expanded(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: context.dynamicHeight(0.01),
-                    ),
-                    Container(
-                      height: context.dynamicHeight(0.08),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: AppColors.orange,
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Sign Up",
-                          style: context.textTheme.bodyLarge?.copyWith(
-                            color: AppColors.background,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        const Expanded(
-                            child: Divider(
-                          thickness: 2,
-                        )),
-                        Text(
-                          "Or",
-                          style: context.textTheme.bodySmall,
-                        ),
-                        const Expanded(
-                            child: Divider(
-                          thickness: 2,
-                        )),
-                      ],
-                    ),
-                    Text(
-                      "Sign up with",
-                      style: context.textTheme.bodyMedium?.copyWith(
-                        color: AppColors.orange,
-                      ),
-                    ),
-                  ],
-                ),
+                flex: 1,
+                child: ButtonAndDivider(),
               ),
               Expanded(
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.facebook,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.facebook,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.facebook,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: context.dynamicHeight(0.01),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text("Already have an account? "),
-                        SizedBox(width: context.dynamicHeight(0.01)),
-                        InkWell(
-                          onTap: () {},
-                          child: Text(
-                            "Sign In",
-                            style: context.textTheme.bodyLarge?.copyWith(
-                              color: AppColors.orange,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                flex: 1,
+                child: SocialMediaAndSignin(),
               )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class SocialMediaAndSignin extends StatelessWidget {
+  const SocialMediaAndSignin({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.facebook,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.facebook,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.facebook,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: context.dynamicHeight(0.01),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(AppStrings.alreadyHaveAccount),
+            SizedBox(width: context.dynamicHeight(0.01)),
+            InkWell(
+              onTap: () {},
+              child: Text(
+                AppStrings.signIn,
+                style: context.textTheme.bodyLarge?.copyWith(
+                  color: AppColors.orange,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ButtonAndDivider extends StatelessWidget {
+  const ButtonAndDivider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: context.dynamicHeight(0.01),
+        ),
+        Container(
+          height: context.dynamicHeight(0.08),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: AppColors.orange,
+          ),
+          child: Center(
+            child: Text(
+              AppStrings.signUp,
+              style: context.textTheme.bodyLarge?.copyWith(
+                color: AppColors.background,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            const Expanded(
+                child: Divider(
+              thickness: 2,
+            )),
+            Text(
+              AppStrings.or,
+              style: context.textTheme.bodySmall,
+            ),
+            const Expanded(
+                child: Divider(
+              thickness: 2,
+            )),
+          ],
+        ),
+        Text(
+          AppStrings.signUpWith,
+          style: context.textTheme.bodyMedium?.copyWith(
+            color: AppColors.orange,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextfieldArea extends StatelessWidget {
+  const TextfieldArea({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        CustomTextField(hintText: AppStrings.fullName, icon: Icons.person_outline),
+        CustomTextField(hintText: AppStrings.email, icon: Icons.mail_outline),
+        CustomTextField(hintText: AppStrings.phone, icon: Icons.phone_outlined),
+        CustomTextField(hintText: AppStrings.password, icon: Icons.lock_outline),
+      ],
+    );
+  }
+}
+
+class HeaderLogoText extends StatelessWidget {
+  const HeaderLogoText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundColor: AppColors.orange,
+          radius: 45,
+          child: Text(
+            AppStrings.logo,
+            style: context.textTheme.headlineLarge?.copyWith(
+              color: AppColors.background,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: context.dynamicHeight(0.05),
+        ),
+        Text(
+          AppStrings.signUp,
+          style: context.textTheme.headlineSmall?.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          AppStrings.findYourDream,
+          style: context.textTheme.bodyLarge,
+        ),
+      ],
     );
   }
 }
